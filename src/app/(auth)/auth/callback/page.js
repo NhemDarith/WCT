4aeb6d56de85f6829b2f3237e6ce27881e77a647
Nhem Darith <nhemdarith20@gmail.com>
2026-01-17 14:21:04 +0700

    fix

--- src/app/(auth)/auth/callback/page.js
+++ src/app/(auth)/auth/callback/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/app/supabase";
+import { supabase } from "@/lib/supabase";
 import Spinner from "@/components/spinner";
 
 
